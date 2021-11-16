355beabe95353f28e52bf66cc5aa4e0c973cb4ea
kaveesha31 <kaveesha.rathnaka@gmail.com>
2021-11-16 01:50:57 -0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dinithi Samindani
         </a>
       </header>
     </div>

916c997eccb2c6c21e03ba06237b0a4d69e60ec4
kaveesha31 <kaveesha.rathnaka@gmail.com>
2021-11-22 17:36:19 -0800

    Home

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -66,34 +66,115 @@ const Home = () => {
                                     magna aliqua.
                                 </p>
                                 <div className="row">
-                                    <div className="col-lg-6">
+                                    <div className="col-lg-12">
                                         <ul>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 May 1995</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.example.com</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+123 456 7890</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>City:</strong> <span>New York, USA</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bsc (Hons) in Information Technology and Management</span></li>
                                         </ul>
                                     </div>
                                     <div className="col-lg-6">
                                         <ul>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>PhEmailone:</strong> <span>email@example.com</span></li>
-                                            <li><i className="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>15 Dec 1997</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Phone:</strong> <span> 0717211257 / 0788067416</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Address:</strong> <span>No. 5D, Sumedha Mawatha,Ganemulla</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Website:</strong> <span>dinithisamindanipriyashiroma.web.app</span></li>
+                                            <li><i className="bi bi-chevron-right"></i> <strong>Email:</strong> <span>dinithisamindani@gmail.com</span></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-lg-6">
+                                        <ul>
+                                            {/* <li><i className="bi bi-chevron-right"></i> <strong>Age:</strong> <span>30</span></li> */}
                                         </ul>
                                     </div>
                                 </div>
-                                <p>
+                                {/* <p>
                                     Officiis eligendi itaque labore et dolorum mollitia officiis optio vero. Quisquam sunt adipisci omnis et ut. Nulla accusantium dolor incidunt officia tempore. Et eius omnis.
                                     Cupiditate ut dicta maxime officiis quidem quia. Sed et consectetur qui quia repellendus itaque neque. Aliquid amet quidem ut quaerat cupiditate. Ab et eum qui repellendus omnis culpa magni laudantium dolores.
-                                </p>
+                                </p> */}
                             </div>
                         </div>
 
                     </div>
                 </section>
 
-                <section id="facts" className="facts">
+
+                <section id="resume" className="resume">
+                    <div className="container">
+
+                        <div className="section-title">
+                            <h2>Resume</h2>
+                            {/* <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> */}
+                        </div>
+
+                        <div className="row">
+                            <div className="col-lg-6" data-aos="fade-up">
+                                <h3 className="resume-title">Sumary</h3>
+                                <div className="resume-item pb-0">
+                                    <h4>Dinithi Samindani</h4>
+                                    <p><em>A passionate and enthusiastic individual with good
+                                        interpersonal and communication skills along with the
+                                        ability to build effective relationships among co-workers
+                                        and clients while managing the projects from the concept
+                                        unto the completion.
+                                    </em></p>
+                                    <ul>
+                                        <li>No. 5D, Sumedha Mawatha,Ganemulla</li>
+                                        <li>0717211257 / 0788067416</li>
+                                        <li>dinithisamindani@gmail.com</li>
+                                    </ul>
+                                </div>
+
+                                <h3 className="resume-title">Education</h3>
+                                <div className="resume-item">
+                                    <h4>Bsc (Hons) in Information Technology and Management</h4><br/>
+                                    <h5>2017 - Present (Expected in 2021)</h5><br/>
+                                    <br/><p><em>University of Moratuwa - Faculty of Information Technology</em></p><br/>
+                                    <p><em>Current GPA - 3.22</em></p><br/>
+                                    {/* <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p> */}
+                                </div>
+                                <div className="resume-item">
+                                    <h4>B.A in English (General) External Degree</h4><br/>
+                                    <h5>2018 - Present (Expected in 2022)</h5><br/>
+                                    <br/><p><em>University of Sri Jayewardenepura</em></p><br/>
+                                    {/* <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p> */}
+                                </div>
+                                <div className="resume-item">
+                                    <h4>Diploma in Translation and Interpretation</h4><br/>
+                                    <h5>2017</h5><br/>
+                                    <br/><p><em>University of Kelaniya</em></p><br/>
+                                    {/* <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p> */}
+                                </div>
+                            </div>
+                            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+                                <h3 className="resume-title">Professional Experience</h3>
+                                <div className="resume-item">
+                                    <h4>Associate Business Analyst.</h4>
+                                    <h5>May 2021 - Present (Part time)</h5>
+                                    <p><em>Agrithmics (Pvt) Ltd</em></p>
+                                    <ul>
+                                        <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
+                                        <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
+                                        <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
+                                        <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
+                                    </ul>
+                                </div>
+                                <div className="resume-item">
+                                    <h4>Software Engineer Intern.</h4>
+                                    <h5>July 2020 - January 2021</h5>
+                                    <p><em>Agrithmics (Pvt) Ltd</em></p>
+                                    <ul>
+                                        <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
+                                        <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
+                                        <li>Recommended and consulted with clients on the most appropriate graphic design</li>
+                                        <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+
+                {/* <section id="facts" className="facts">
                     <div className="container">
 
                         <div className="section-title">
@@ -138,7 +219,7 @@ const Home = () => {
                         </div>
 
                     </div>
-                </section>
+                </section> */}
 
                 <section id="skills" className="skills section-bg">
                     <div className="container">
@@ -200,71 +281,6 @@ const Home = () => {
 
                             </div>
 
-                        </div>
-
-                    </div>
-                </section>
-
-                <section id="resume" className="resume">
-                    <div className="container">
-
-                        <div className="section-title">
-                            <h2>Resume</h2>
-                            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-                        </div>
-
-                        <div className="row">
-                            <div className="col-lg-6" data-aos="fade-up">
-                                <h3 className="resume-title">Sumary</h3>
-                                <div className="resume-item pb-0">
-                                    <h4>Dinithi Samindani</h4>
-                                    <p><em>Innovative and deadline-driven Graphic Designer with 3+ years of experience designing and developing user-centered digital/print marketing material from initial concept to final, polished deliverable.</em></p>
-                                    <ul>
-                                        <li>Portland par 127,Orlando, FL</li>
-                                        <li>(123) 456-7891</li>
-                                        <li>alice.barkley@example.com</li>
-                                    </ul>
-                                </div>
-
-                                <h3 className="resume-title">Education</h3>
-                                <div className="resume-item">
-                                    <h4>Master of Fine Arts &amp; Graphic Design</h4>
-                                    <h5>2015 - 2016</h5>
-                                    <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                                    <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
-                                </div>
-                                <div className="resume-item">
-                                    <h4>Bachelor of Fine Arts &amp; Graphic Design</h4>
-                                    <h5>2010 - 2014</h5>
-                                    <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                                    <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur neque etlon sader mart dila</p>
-                                </div>
-                            </div>
-                            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-                                <h3 className="resume-title">Professional Experience</h3>
-                                <div className="resume-item">
-                                    <h4>Senior graphic design specialist</h4>
-                                    <h5>2019 - Present</h5>
-                                    <p><em>Experion, New York, NY </em></p>
-                                    <ul>
-                                        <li>Lead in the design, development, and implementation of the graphic, layout, and production communication materials</li>
-                                        <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of the project. </li>
-                                        <li>Supervise the assessment of all graphic materials in order to ensure quality and accuracy of the design</li>
-                                        <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000</li>
-                                    </ul>
-                                </div>
-                                <div className="resume-item">
-                                    <h4>Graphic design specialist</h4>
-                                    <h5>2017 - 2018</h5>
-                                    <p><em>Stepping Stone Advertising, New York, NY</em></p>
-                                    <ul>
-                                        <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and advertisements).</li>
-                                        <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                                        <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                                        <li>Created 4+ design presentations and proposals a month for clients and account managers</li>
-                                    </ul>
-                                </div>
-                            </div>
                         </div>
 
                     </div>
@@ -430,7 +446,7 @@ const Home = () => {
                     </div>
                 </section>
 
-                <section id="testimonials" className="testimonials section-bg">
+                {/* <section id="testimonials" className="testimonials section-bg">
                     <div className="container">
 
                         <div className="section-title">
@@ -511,7 +527,7 @@ const Home = () => {
                         </div>
 
                     </div>
-                </section>
+                </section> */}
 
                 <section id="contact" className="contact">
                     <div className="container">
